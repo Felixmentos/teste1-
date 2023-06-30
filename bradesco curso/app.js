b0ef50cfd4fbd9dd5e8b98241029787a08c4d82f
Adryel Ferreira Félix <127853830+Felixmentos@users.noreply.github.com>
2023-06-30 13:56:13 -0300

    Update app.js
    resolvi fds kllkkklkkkk

--- bradesco curso/app.js
+++ bradesco curso/app.js
@@ -1,17 +1,16 @@
 'use strick';
 
-const switcher = document.querrySelector('.btn');
+const switcher = document.querySelector('.btn');
 switcher.addEventListener('click', function() {
-    document.body.classList.toggle('dak-theme')
-});
+    document.body.classList.toggle('dark-theme')
 
     var className = document.body.className;
     if(className == "light-them") {
         this.textContent == "Dark";
-    else {
+    }else {
+    
         this.textContent = "Light";
     }
-});
 
 console.log('current class name:' + className); 
 });
